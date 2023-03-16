9ab7c2efb586f7cd68bd30f783661c80d130f03e
jace <103141574+SKALE-jace@users.noreply.github.com>
2023-03-16 13:25:39 -0500

    removed unneeded comments

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,3 @@
-// require("@nomicfoundation/hardhat-toolbox");
-
-// /** @type import('hardhat/config').HardhatUserConfig */
-// module.exports = {
-//   solidity: "0.8.9",
-// };
-
-
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-etherscan");
 const dotenv = require("dotenv");
